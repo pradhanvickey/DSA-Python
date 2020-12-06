873e357a4cd1e46fc2c742c61aa0fb1ea9d17149
pradhanvickey <pradhanvickey@gmail.com>
2020-12-06 10:49:34 +0530

    Inserting element at The Beginning of Linked list

---
+++ LinkList/insertion_at_the_begining.py
@@ -0,0 +1,33 @@
+class Node:
+  def __init__(self, data):
+    self.data = data
+    self.next = None
+
+class LinkList:
+  def __init__(self):
+    self.head = None
+
+  def insert_at_begin(self, data):
+    new_node = Node(data)
+    if self.head is None:
+      self.head = new_node
+    else:
+      new_node.next = self.head
+      self.head = new_node
+
+  def print_element(self):
+    temp = self.head
+    while temp is not None:
+      end_str = " => "
+      if temp.next is None:
+        end_str = "\n"        
+      print(temp.data, end=end_str)
+      temp = temp.next
+
+if __name__ == '__main__':
+  l1 = LinkList()
+  #inserting element in LinkList
+  for i in range(1, 5):
+    l1.insert_at_begin(i)
+  #printing element in LinkList
+  l1.print_element()
