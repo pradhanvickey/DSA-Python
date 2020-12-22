ffc080a6e1909bccc9974378b2736ba6aab0e4a6
pradhanvickey <pradhanvickey@gmail.com>
2020-12-22 09:13:06 +0530

    Nearest greatest to left.

---
+++ Stack/nearest_greatest_to_left.py
@@ -0,0 +1,29 @@
+# NGL | Nearest Greater to left
+# complexity: o(n)
+
+def nearest_greatest_to_left(arr, size):
+    stack = []
+    res = []
+    
+    for i in range(0, size):
+        found = -1
+        while len(stack) > 0:
+            value = stack[-1]
+            if arr[i] < value:
+                res.append(value)
+                stack.append(arr[i])
+                found = 1
+                break
+            else:
+                stack.pop()
+                
+        if found == -1:
+            res.append(-1)
+            stack.append(arr[i])
+            
+    print(res)
+
+if __name__ == '__main__':    
+    arr = [1, 3, 2, 5]
+    size = len(arr)
+    nearest_greatest_to_left(arr, size)
