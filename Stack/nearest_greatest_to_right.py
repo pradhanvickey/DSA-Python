627e7ccc520781d7bbe17debf1e3b945a65301ee
pradhanvickey <pradhanvickey@gmail.com>
2020-12-22 09:13:30 +0530

    Nearest greatest to right.

---
+++ Stack/nearest_greatest_to_right.py
@@ -0,0 +1,30 @@
+# NGR | Nearest Greater to right | Next Largest Element
+# complexity: o(n)
+
+def nearest_greatest_to_right(arr, size):
+    stack = []
+    res = []
+    
+    for i in range(size, -1, -1):
+        found = -1
+        while len(stack) > 0:
+            value = stack[-1]
+            if arr[i] < value:
+                res.append(value)
+                stack.append(arr[i])
+                found = 1
+                break
+            else:
+                stack.pop()
+                
+        if found == -1:
+            res.append(-1)
+            stack.append(arr[i])
+
+    res.reverse()
+    print(res)
+
+if __name__ == '__main__':    
+    arr = [1, 3, 2, 4]
+    size = len(arr) -1
+    nearest_greatest_to_right(arr, size)
